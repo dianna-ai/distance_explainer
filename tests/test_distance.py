0e160b84a3f20b0bbad12f2435a958710182b90a
Christiaan Meijer <c.meijer@esciencecenter.nl>
2023-07-11 14:00:24 +0200

    add test for explainer exact output

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -33,6 +33,35 @@ def test_runs_with_dummy_outputs_correct_shape(set_all_the_seeds: Callable, dumm
     assert saliency.shape == (1,) + input_arr.shape[:2] + (1,)
 
 
+def test_runs_with_dummy_exact_output(set_all_the_seeds: Callable, dummy_model: Callable):
+    """Code should run without errors and have the correct output shape."""
+    config = get_default_config()
+
+    embedded_reference = np.random.randn(1, DUMMY_EMBEDDING_DIMENSIONALITY)
+    input_arr = np.random.random((224, 224, 3))
+    explainer = DistanceExplainer(mask_selection_range_max=config.mask_selection_range_max,
+                                  mask_selection_range_min=config.mask_selection_range_min,
+                                  mask_selection_negative_range_max=config.mask_selection_negative_range_max,
+                                  mask_selection_negative_range_min=config.mask_selection_negative_range_min,
+                                  n_masks=config.number_of_masks,
+                                  axis_labels={2: 'channels'},
+                                  preprocess_function=None,
+                                  feature_res=config.feature_res,
+                                  p_keep=config.p_keep)
+
+    saliency, value = explainer.explain_image_distance(dummy_model, input_arr, embedded_reference)
+
+    output_npz = './test_data/test_runs_with_dummy_exact_output.npz'
+    np.savez(output_npz, saliency, value)
+    loaded = np.load(output_npz)
+    [expected_saliency, expected_value] = [loaded[k] for k in loaded.files]
+
+    assert np.allclose(expected_saliency, saliency)
+    assert np.allclose(expected_value, value)
+
+
+
+
 @pytest.fixture()
 def dummy_model() -> Callable:
     """Get a dummy model that returns a random embedding for every input in a batch."""
